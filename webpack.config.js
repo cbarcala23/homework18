01ceed297b8145112affc1b557bbc5798b4b1ec6
itadmin <itadmin@computer.hsd1.wa.comcast.net>
2020-02-01 20:03:40 -0800

    omg

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./public/index.js",
+    db: "./public/db.js",
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new SWPrecacheWebpackPlugin({
+      cacheId: "my-domain-cache-id",
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: "service-worker.js",
+      minify: true,
+      staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+    }),
+    new WebpackPwaManifest({
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
+      description: "An application for budget",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      "theme-color": "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("./public/assets/images/icons/icon-192x192.png"),
+          sizes: [192],
+          destination: path.join("assets", "icons")
+        }
+      ]
+    })
+  ]
+};
+module.exports = config;
